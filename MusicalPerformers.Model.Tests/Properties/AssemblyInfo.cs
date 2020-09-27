1cc78f12cb1f31648bd1880b1181dc3c9d781b54
Lev4and <54065747+Lev4and@users.noreply.github.com>
2020-09-27 23:04:09 +0500

    Создание проектов. Подключение ссылок и необходимых библиотек.

---
+++ MusicalPerformers.Model.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MusicalPerformers.Model.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MusicalPerformers.Model.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("60e6a932-407f-4c2f-9ea2-b7326298e42d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
